b162386beb4b94dec53c21ee29ca851e6e0bb016
Deejay <ddhananjayrao@gmail.com>
2022-05-02 11:07:28 +0530

    digi-clock

---
+++ app.js
@@ -0,0 +1,39 @@
+function showTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    
+    const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    var war = days[dateTime.getDay()];
+
+    var dinank = dateTime.getDate();
+
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+    var mahina = months[dateTime.getMonth()];
+
+
+    var saal = dateTime.getFullYear();
+    var session = document.getElementById('session');
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    hrs = (hrs < 10) ? "0" + hrs : hrs;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    document.getElementById('hrs').innerHTML = hrs;
+    document.getElementById('min').innerHTML = min;
+    document.getElementById('sec').innerHTML = sec;
+    document.getElementById('war').innerHTML = war;
+    document.getElementById('dinank').innerHTML = dinank;
+    document.getElementById('mahina').innerHTML =mahina;
+    document.getElementById('saal').innerHTML = saal;
+}
+
+setInterval(showTime, 10);
